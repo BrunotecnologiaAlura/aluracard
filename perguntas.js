c996108dad8f262023a15663e96c9a7da21c01f9
BrunotecnologiaAlura <00001092058886sp@al.educacao.sp.gov.br>
2024-11-06 08:26:14 -0300

    Bruno

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável? ',
+    'Uma variável é um lugar onde guardamos informações que podem mudar enquanto o programa roda.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
